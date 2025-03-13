6b41ab6c38d7ccac443c2023925c715ce30a4665
zallengrad <zallengrad66@gmail.com>
2025-03-13 12:09:15 +0700

    todo apps dengan web storage

--- todoapps/script.js
+++ todoapps/script.js
@@ -2,6 +2,43 @@ function generateID() {
     return +new Date();
 }
 
+const SAVED_EVENT = 'saved-todo';
+const STORAGE_KEY = 'TODO_APPS';
+ 
+function isStorageExist() /* boolean */ {
+  if (typeof (Storage) === undefined) {
+    alert('Browser kamu tidak mendukung local storage');
+    return false;
+  }
+  return true;
+}
+
+document.addEventListener(SAVED_EVENT, function () {
+    console.log(localStorage.getItem(STORAGE_KEY));
+  });
+
+function saveData() {
+    if (isStorageExist()) {
+      const parsed = JSON.stringify(todos);
+      localStorage.setItem(STORAGE_KEY, parsed);
+      document.dispatchEvent(new Event(SAVED_EVENT));
+    }
+  }
+
+  function loadDataFromStorage() {
+    const serializedData = localStorage.getItem(STORAGE_KEY);
+    let data = JSON.parse(serializedData);
+   
+    if (data !== null) {
+      for (const todo of data) {
+        todos.push(todo);
+      }
+    }
+   
+    document.dispatchEvent(new Event(RENDER_EVENT));
+  }
+
+
 function generatetodoObject(id, task, timeStamp, isCompleted) {
     return {
         id,
@@ -94,6 +131,7 @@ function addTodo() {
     todos.push(todoObject);
 
     document.dispatchEvent(new Event(RENDER_EVENT));
+    saveData();
 }
 
 
@@ -112,6 +150,7 @@ function removeTaskFromCompleted(todoId /* HTMLELement */) {
     todos.splice(todoTarget, 1);
   
     document.dispatchEvent(new Event(RENDER_EVENT));
+    saveData();
   }
   
   function undoTaskFromCompleted(todoId /* HTMLELement */) {
@@ -120,6 +159,7 @@ function removeTaskFromCompleted(todoId /* HTMLELement */) {
   
     todoTarget.isCompleted = false;
     document.dispatchEvent(new Event(RENDER_EVENT));
+    saveData();
   }
 
   
@@ -129,6 +169,10 @@ document.addEventListener('DOMContentLoaded', function () {
         event.preventDefault();
         addTodo();
     });
+
+    if (isStorageExist()) {
+        loadDataFromStorage();
+      }
 });
 
 
